bfa0b02e38c9c68cca267cd34327ce34d4bb88a0
Jorge Matos <jisapjisap@gmail.com>
2025-03-11 16:02:27 +0100

    Add initial assets, configure Tailwind CSS, and implement layout components

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="font-primary">Este texto debería usar DM Sans</h1>
+      <p className="font-secondary">Este texto debería usar Barlow</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: ["DM Sans", "sans-serif"], // Al definir los nombres de las fuentes directamente, Tailwind generará las clases correctamente.
+      secondary: ["Barlow", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#121315",
+        secondary: "#666666",
+        accent: "#ffca3b",
+        border: "#d7d7d7",
+      },
+      boxShadow: {
+        custom: "0px 4px 54px 10px rgba(18, 19, 21, 0.06)"
+      },
+      backgroundImage:{
+        hero: "url('/assets/images/hero/bg.jpg')",
+      }
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate")
+  ],
+}
+
