e870260a6553c47ee1914fde00b54b63931953b7
Jorge Matos <jisapjisap@gmail.com>
2025-03-13 19:19:42 +0100

    Enhance Header component with navigation layout and quote button; integrate icon for improved UX

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { RiArrowRightUpLine } from "react-icons/ri";
 import { Link as ScrollLink } from "react-scroll";  
 
 const links = [
@@ -29,23 +30,43 @@ const Header = () => {
   return (
     <header className="bg-primary py-4 sticky top-0">
       <div className="container mx-auto">
-        <ul className="flex gap-12 text-white">
-          {links.map((link, index) => {
-            return (
-              <li key={index}>
-                <ScrollLink 
-                  to={link.path}
-                  smooth
-                  spy 
-                  className="cursor-pointer"  
-                  activeClass="text-accent"
+
+      <div className="flex items-center justify-between">
+        {/* logo */}
+        <div>logo</div>
+
+        {/* nav */}
+        <nav className="hidden xl:flex items-center gap-12">
+          <ul className="flex">
+            {links.map((link, index) => {
+              return (
+                <li 
+                  key={index} 
+                  className="text-white text-sm uppercase font-primary font-medium tracking-[1.2px] after:content-['/'] after:mx-4 last:after:content-none"
                 >
-                  {link.name}
-                </ScrollLink>
-              </li>
-            )
-          })}
-        </ul>
+                  <ScrollLink 
+                    to={link.path}
+                    smooth
+                    spy 
+                    className="cursor-pointer"  
+                    activeClass="text-accent"
+                  >
+                    {link.name}
+                  </ScrollLink>
+                </li>
+              )
+            })}
+          </ul>
+          <button className="w-[200px] h-[54px] py-[5px] pl-[10px] pr-[5px] flex items-center justify-between min-w-[200px] bg-white group">
+            <div className="flex-1 text-center tracking-[1.2px] font-primary font-bold text-primary text-sm uppercase">
+              Get a quote
+            </div>
+            <div className="w-11 h-11 bg-primary flex items-center justify-center">
+              <RiArrowRightUpLine className="text-white text-xl group-hover:rotate-45 transition-all duration-200" />
+            </div>
+          </button>
+        </nav>
+      </div>
       </div>
     </header>
   )
