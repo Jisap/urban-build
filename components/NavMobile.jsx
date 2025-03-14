3e37c05fbcdf07e6ff66f96310c0fc7243bf34f7
Jorge Matos <jisapjisap@gmail.com>
2025-03-14 14:23:52 +0100

    Implement mobile navigation component with menu toggle and links

--- components/NavMobile.jsx
+++ components/NavMobile.jsx
@@ -1,12 +1,68 @@
+"use client"
 
+import { RiMenu3Fill } from "react-icons/ri";
+import { 
+  Sheet, 
+  SheetContent, 
+  SheetDescription, 
+  SheetHeader, 
+  SheetTitle, 
+  SheetTrigger 
+} from "./ui/sheet";
+import Logo from "./Logo";
+import Socials from "./Socials";
+import { Link as ScrollLink} from "react-scroll";
+import { useState } from "react";
 
-
+const links = [
+  {
+    name: "home",
+    path: "home",
+  },
+  {
+    name: "about",
+    path: "about",
+  },
+  {
+    name: "services",
+    path: "services",
+  },
+  {
+    name: "projects",
+    path: "projects",
+  },
+  {
+    name: "contact",
+    path: "contact",
+  }
+]
 
 const NavMobile = () => {
+
+  const[isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      NavMobile
-    </div>
+    <Sheet
+      open={isOpen}
+      onOpenChange={setIsOpen}
+    >
+      <SheetTrigger 
+        asChild 
+        onClick={() => setIsOpen(true)}
+        className= "text-white flex items-center justify-center text-3xl">
+        <RiMenu3Fill />
+      </SheetTrigger>
+      <SheetContent className="bg-primary border-none text-white">
+        <div>
+          <SheetHeader>
+            <SheetTitle><Logo /></SheetTitle>
+            <SheetDescription>
+              Navigation menu
+            </SheetDescription>
+          </SheetHeader>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
