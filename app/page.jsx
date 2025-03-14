6976ea207eec23fa6f2e3d08fc7857326fbcb312
Jorge Matos <jisapjisap@gmail.com>
2025-03-14 14:53:20 +0100

    header animated vs static

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,6 @@
+"use client"
+
+
 import Hero from "@/components/Hero"
 import Topbar from "@/components/Topbar"
 import Header from "../components/Header"
@@ -9,13 +12,36 @@ import Testimonials from "@/components/Testimonials"
 import Faq from "@/components/Faq"
 import Contact from "@/components/Contact"
 import Footer from "@/components/Footer"
+import { useEffect, useState } from "react"
 
 
 const Home = () => {
+
+  const [headerActive, setHeaderActive] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHeaderActive(window.scrollY > 200);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+
+  },[])
+
   return (
     <div>
       <Topbar />
-      <Header />
+      {/* static header */}
+      <div className="relative z-10">
+        <Header  />
+      </div>
+      {/* animated header */} 
+      <div className={`w-full transition-transform duration-500 fixed top-0 left-0 z-50 ${headerActive ? "translate-y-0" : "-translate-y-full"}`}>
+        <Header />
+      </div>
       <Hero />
       <About />
       <Stats />
