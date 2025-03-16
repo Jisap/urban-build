f2de53e52178c23505a9dafefb600902d0265f21
Jorge Matos <jisapjisap@gmail.com>
2025-03-16 18:22:39 +0100

    Update Hero component layout for improved text alignment and responsiveness

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       {/* overlay */}
       <div className="absolute inset-0 bg-gradient-to-l from-black/0 via-black/50 to-black/70 z-10"></div>
       <div className="container mx-auto h-full flex items-center">
-        <div className="z-20 text-white">
+        <div className="z-20 text-white text-center xl:text-left mx-auto xl:mx-0 flex flex-col items-center xl:items-start max-w-[608px]">
           <h1 className="h1 text-white mb-4">
             <span className="text-accent">Building</span> robust lasting solutions
           </h1>
