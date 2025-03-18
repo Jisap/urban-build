ef4733054ed33e7716441149140890bd4382fe85
Jorge Matos <jisapjisap@gmail.com>
2025-03-18 14:03:04 +0100

    Add Label and Card components; update Services component with tabs and service data

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,11 +1,21 @@
+"use client"
+
 import { useState } from "react"
-import { Tabs, TabsContent, TabsTrigger } from "./ui/tabs"
+
 import Image from "next/image"
 import Button from "./Button"
 import { PiWallFill, PiPaintRollerFill, PiWrenchFill, PiUserGearFill } from "react-icons/pi"
 
 
-const services = [
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+
+
+
+
+const serviceData = [
   {
     name: "construction",
     icon: <PiWallFill />,
@@ -81,10 +91,48 @@ const services = [
 ]
 
 const Services = () => {
+
+  const [activeTab, setActiveTab] = useState("construction")
+
   return (
-    <div className="h-screen bg-green-200" id="services">
-      Services
-    </div>
+    <section className="pt-16 xl:pt-32" id="services">
+      <div className="container mx-auto">
+        <div>
+          <h2 className="h2 mb-3">
+            Solution We Provide
+          </h2>
+          <p className="mb-11 max-w-[480px] mx-auto">
+            Offering tailored construction solutions, from planning to completion, with a focus on quality, precision, and innovation.
+          </p>
+        </div>
+
+        {/* tabs */}
+        <Tabs defaultValue="construction" className="h-full">
+          <TabsList className="grid w-full grid-cols-2">
+            {serviceData.map((item) => {
+              return (
+                <TabsTrigger 
+                  key={item.name} 
+                  value={item.name} 
+                  className="w-full rounded-none h-[100px] flex items-center relative shadow-custom p-0 outline-none"
+                >
+                  <div>
+
+                  </div>
+                  {item.name}
+                </TabsTrigger>
+              )
+            })}
+          </TabsList>
+          <TabsContent value="account">
+            Account
+          </TabsContent>
+          <TabsContent value="password">
+            Password
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
   )
 }
 
