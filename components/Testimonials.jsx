09e900f89b5a20695ad21f95a950918ed288b4b7
Jorge Matos <jisapjisap@gmail.com>
2025-03-23 17:55:22 +0100

    Enhance Testimonials component: restructure layout and add content with button for client engagement

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,9 +1,30 @@
+import Image from "next/image"
+import Button from "./Button"
+import Pretitle from "./Pretitle"
+
+
+
 
 
 const Testimonials = () => {
   return (
-    <div className="h-screen bg-teal-200">
-      Testimonials
+    <div className="pt-16 xl:pt-32">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row relative">
+          {/* text */}
+          <div className="flex-1">
+            <h2 className="h2 mb-6">Built On Trust, Proven By Results</h2>
+            <p className="mb-10 max-w-[420px]">
+              From home to commercial spaces, our clients share their experiences of working wiht us. See how we&apos;ve helped them bring their dreams to live with expert craftmanship.
+            </p>
+            <Button 
+              text="Work with us"
+            />
+          </div>
+          {/* img & slider */}
+          <div className="flex-1">img & slider</div>
+        </div>
+      </div>
     </div>
   )
 }
