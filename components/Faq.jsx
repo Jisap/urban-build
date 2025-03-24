668bdc50df71c0e1a1882fe803e571d9317a41ec
Jorge Matos <jisapjisap@gmail.com>
2025-03-24 16:02:55 +0100

    Add FAQ component: implement FAQ section with animated items for enhanced user experience

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -1,8 +1,60 @@
+import { fadeIn } from "@/variants"
+import Pretitle from "./Pretitle"
+import { motion } from "framer-motion"
 
+const faqItemsData = [
+  {
+    title: "How long does a construction project ussually take?",
+    description: "Timelines vary based on project size complexity."
+  },
+  {
+    title: "Do I need permits for my project?",
+    description: "he permits are managed by our team to ensure everything complies with local regulations."
+  },
+  {
+    title: "What materials do you use?",
+    description: "We use high-quality, durable materials tailored to the specific needs of each project, balancing aesthetics, longevity, and budget."
+  },
+  {
+    title: "Can I make changes after construction starts?",
+    description: "Yes, changes can be made, though they may impact the timeline and cost. We’ll review and approve adjustments with you."
+  },
+  {
+    title: "How much will my construction project cost?",
+    description: "Costs depend on project size, design, and materials. We provide transparent quotes and work within your budget."
+  },
+  {
+    title: "How do you ensure quality and safety on-site?",
+    description: "We follow strict safety protocols, perform regular inspections, and work only with trusted, skilled professionals to guarantee quality and safety."
+  },
+];
+
+//animation variants for FAQ items
+const faqItemVariants = {
+  hidden: { opacity: 0, y: 30 },
+  visible: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: index * 0.1, 
+      duration: 0.3 // staggered animation
+    }
+  })
+}
 
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <section className="pt-16 xl:pt-32">
+      <div className="container mx-auto">
+        {/* text */}
+        <div>
+          <Pretitle text="Faq" center />
+          <h2 className="h2">Got Questions?</h2>
+        </div>
+        {/* faq items */}
+        <div>faq items</div>
+      </div>
+    </section>
   )
 }
 
