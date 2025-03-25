49887310ba768b0b42a95d3dac8940d240e1b06f
Jorge Matos <jisapjisap@gmail.com>
2025-03-25 16:28:21 +0100

    Enhance Contact and Form components: improve layout, styling, and input handling for better user experience

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,9 +10,10 @@ const Contact = () => {
     <div className="pt-16 xl:pt-32" id="contact">
       <div className="container mx-auto">
         <div className="w-full xl:h-[730px] shadow-custom p-4 xl:p-8 xl:px-[90px] xl:py-[36px] border-t-4 border-accent">
-          <div className="flex flex-col xl:flex-row h-full gap-[40px] xl:gap-[90px]">
+          <div className="flex flex-col xl:flex-row h-full gap-[40px] xl:gap-[90px]">         
             {/* info */}
-            <div className="w-full">info</div>
+            <div className="w-full xl:max-w-[380px] xl:pr-[70px] xl:border-r xl:border-border/40 h-[640px]">info</div>
+            
             {/* form */}
             <div className="flex-1">
               <h2 className="h2 mb-3">
@@ -21,8 +22,6 @@ const Contact = () => {
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Pellentesque et odio sed est pellentesque gravida. 
-                Sed pharetra, felis ac mattis commodo, massa risus 
-                tincidunt quam, in consequat tortor libero nec magna.
               </p>
               <Form />
             </div>
--- components/Form.jsx
+++ components/Form.jsx
@@ -15,42 +15,58 @@ import Button from "./Button"
 
 function Form() {
   return (
-    <form>
-      <Input 
-        type="fullname"
-        placeholder="Full Name"
-      />
-      <Input 
-        type="email"
-        placeholder="Email address"
-      />
-      <div>
-        <Input 
-          type="phone"
-          placeholder="Phone number"
+    <form className="flex flex-col">
+      <div className="flex flex-col gap-20px mb-[20px]">
+        <Input
+          type="fullname"
+          placeholder="Full Name"
         />
-        <Select>
-          <SelectTrigger className="w-full rounded-none h-[54px] text-secondary outline-none">
-            <SelectValue placeholder="Select a service"></SelectValue>
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Select a service</SelectLabel>
-              <SelectItem value="construction">
-                Construction
-              </SelectItem>
-              <SelectItem value="renovation">
-                Renovation
-              </SelectItem>
-              <SelectItem value="restoration">
-                Restoration
-              </SelectItem>
-              <SelectItem value="Consulting">
-                Consulting
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        <Input
+          type="email"
+          placeholder="Email address"
+        />
+        <div>
+          <Input
+            type="phone"
+            placeholder="Phone number"
+          />
+          <Select>
+            <SelectTrigger className="w-full rounded-none h-[54px] text-secondary outline-none">
+              <SelectValue placeholder="Select a service"></SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Select a service</SelectLabel>
+                <SelectItem value="construction">
+                  Construction
+                </SelectItem>
+                <SelectItem value="renovation">
+                  Renovation
+                </SelectItem>
+                <SelectItem value="restoration">
+                  Restoration
+                </SelectItem>
+                <SelectItem value="Consulting">
+                  Consulting
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        {/* textarea */}
+        <Textarea
+          className="h-[180px] resize-none rounded-none"
+          placeholder="Enter your message"
+        />
+        {/* btn */}
+        <Button
+          text="Send Message"
+        >
+          Send Message
+        </Button>
       </div>
     </form>
   )
