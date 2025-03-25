0d06f69b5607f21cf6602924cec65f10e3d496b2
Jorge Matos <jisapjisap@gmail.com>
2025-03-25 17:38:54 +0100

    Enhance Textarea component: update styling for improved accessibility and user experience

--- components/ui/textarea.jsx
+++ components/ui/textarea.jsx
@@ -10,7 +10,7 @@ function Textarea({
     (<textarea
       data-slot="textarea"
       className={cn(
-        "border-border bg-white focus:border-accent placeholder:text-muted-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-6 py-4 text-base transition-[color,box-shadow] outline-none  disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "border-border bg-white focus:border-accent text-primary text-base placeholder:text-secondary placeholder:text-base aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3.5 py-4 transition-[color,box-shadow] outline-none  disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props} />)
