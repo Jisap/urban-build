64a582ba62382cbb42f4906f235cac55f2cc9d42
Jorge Matos <jisapjisap@gmail.com>
2025-03-26 14:59:41 +0100

    Enhance Footer component: update layout and add contact and newsletter sections for improved user engagement

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className="mt-16 xl:mt-32 bg-primary">
       <div className="container mx-auto">
-        <div>
+        <div className="py-16 xl:py-[100px] flex flex-col xl:flex-row gap-[60px] xl:gap-[30px]">
           {/* logo & text*/}
           <div className="flex-1">
             <Link href="/" className="flex mb-6">
@@ -24,13 +24,49 @@ const Footer = () => {
             </Link>
             <p className="text-border max-w-[270px]">
               lorem ipsum dolor sit amet consectetur adipisicing elit. fugiat
-              voluptas, quidem, voluptate, consequuntur, velit, placeat
+              voluptas, quidem, voluptate.
             </p>
           </div>
 
           {/* Contact */}
           <div className="flex-1 text-border">
+            <h4 className="h4 text-white mb-10">Contact</h4>
+            <ul>
+              <li className="flex items-center gap-4">
+                <RiMapPin2Fill className="text-accent text-xl"/>
+                <p>1250 Bristol St, San Francisco, CA 94103</p>
+              </li>
+              <li className="flex items-center gap-4">
+                <RiMailFill className="text-accent text-xl" />
+                <p>hi@urbanbuild.com</p>
+              </li>
+              <li className="flex items-center gap-4">
+                <RiPhoneFill className="text-accent text-xl" />
+                <p>+1 (800) 555-1234</p>
+              </li>
+            </ul>
+          </div>
 
+          {/* newsletter */}
+          <div className="flex-1 text-border">
+            <h4 className="h4 text-white mb-10">Newsletter</h4>
+            <p className="mb-9">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              voluptas, quidem, voluptate.
+            </p>
+            {/* input */}
+            <div>
+              <input 
+                type="text"
+                placeholder="Enter your email"
+                className="bg-[#222427] h-16 w-full pl-7 rounded-none outline-none flex items-center"  
+              />
+              <button
+                className="bg-accent w-12 h-12 absolute right-2 top-2 bottom-2 text-priamry text-xl flex items-center justify-center"
+              >
+                <RiArrowRightLine />
+              </button>
+            </div>
           </div>
         </div>
       </div>
