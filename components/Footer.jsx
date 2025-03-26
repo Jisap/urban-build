f9e016f4ba9179b65d3600ff471e7cd8f4669bd8
Jorge Matos <jisapjisap@gmail.com>
2025-03-26 14:36:51 +0100

    Enhance Footer component: replace div with footer element for improved semantic structure and layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,40 @@
+import Image from "next/image"
+import Link from "next/link"
+import { RiArrowRightLine, RiMailFill, RiMapPin2Fill, RiPhoneFill } from "react-icons/ri"
+import Socials from "./Socials"
+
+
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="mt-16 xl:mt-32 bg-primary">
+      <div className="container mx-auto">
+        <div>
+          {/* logo & text*/}
+          <div className="flex-1">
+            <Link href="/" className="flex mb-6">
+              <Image
+                src="/assets/logo.svg"
+                alt="logo"
+                width={230}
+                height={48}
+                className=""
+              />
+            </Link>
+            <p className="text-border max-w-[270px]">
+              lorem ipsum dolor sit amet consectetur adipisicing elit. fugiat
+              voluptas, quidem, voluptate, consequuntur, velit, placeat
+            </p>
+          </div>
+
+          {/* Contact */}
+          <div className="flex-1 text-border">
+
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
