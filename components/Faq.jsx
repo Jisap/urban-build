d53bd56cd73f78ce944c58b47bb2d606199fa3fa
Jorge Matos <jisapjisap@gmail.com>
2025-03-27 16:36:09 +0100

    Enhance Faq component: integrate framer-motion for improved animations and structure

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -48,23 +48,36 @@ const Faq = () => {
     <section className="pt-16 xl:pt-32">
       <div className="container mx-auto">
         {/* text */}
-        <div className="text-center max-w-[540px] mx-auto xl:mb-20">
+        <motion.div 
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.2 }}
+          className="text-center max-w-[540px] mx-auto xl:mb-20"
+        >
           <Pretitle text="Faq" center />
           <h2 className="h2 mb-3">Got Questions? We&apos;ve Got You Covered</h2>
           <p className="mb-11 max-w-[480px] mx-auto">
             From project planning to final touches, we&apos;ve answered the most common questions to help you make informed decisions.
           </p>
-        </div>
+        </motion.div>
         {/* faq items */}
         <ul className="w-full flex flex-col">
           {faqItemsData.map((item, index) => {
             return (
-              <li key={index}>
+              <motion.li 
+                key={index}
+                variants={faqItemVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: false, amount: 0.2 }}
+                custom={index} // pass index to control stagger effect
+              >
                 <FaqItem 
                   title={item.title} 
                   description={item.description} 
                 />
-              </li>
+              </motion.li>
             )
           })}
         </ul>
